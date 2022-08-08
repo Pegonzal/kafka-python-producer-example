c21aa19fc0b2f86fa105e9f2ccaaaa2e3f7396bf
Max Kossatz <mkossatz@redhat.com>
2022-08-08 23:17:40 +0200

    fix loop

--- kafka_python_producer/producer.py
+++ kafka_python_producer/producer.py
@@ -6,6 +6,6 @@ from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers='rover-cluster-kafka-bootstrap:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-for True:
+while True:
     producer.send('new-message-topic', {'foo': 'bar'})
     time.sleep(3)
