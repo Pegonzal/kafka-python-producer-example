b32bca1ab58140042a5088c509763c60024706e6
Max Kossatz <mkossatz@redhat.com>
2022-08-08 23:15:06 +0200

    send endlessly with pause

--- kafka_python_producer/producer.py
+++ kafka_python_producer/producer.py
@@ -1,9 +1,11 @@
 import json
+import time
 
 from kafka import KafkaProducer
 
 
 producer = KafkaProducer(bootstrap_servers='rover-cluster-kafka-bootstrap:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-for _ in range(10):
+for True:
     producer.send('new-message-topic', {'foo': 'bar'})
+    time.sleep(3)
