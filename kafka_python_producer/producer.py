24c01fd01f0326a75553c3b5c8eb7bb57ecfa9d4
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2022-08-09 14:47:59 +0200

    Update producer.py

--- kafka_python_producer/producer.py
+++ kafka_python_producer/producer.py
@@ -7,5 +7,5 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='rover-cluster-kafka-bootstrap:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 while True:
-    producer.send('new-message-topic', {'foo': 'bar'})
+    producer.send('123test', {'foo': 'bar'})
     time.sleep(3)
